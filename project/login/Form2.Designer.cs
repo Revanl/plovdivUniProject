e78dbcff6926d331948be110a26bf9a683aae205
iani <ianigarfalski@gmail.com>
2018-10-01 00:17:39 +0300

    fullprojectWithDB

---
+++ project/login/Form2.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Login
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.xHours = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.xMinutes = new System.Windows.Forms.Label();
+            this.xSeconds = new System.Windows.Forms.Label();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.taskName = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newTaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // xHours
+            // 
+            this.xHours.AutoSize = true;
+            this.xHours.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xHours.Location = new System.Drawing.Point(89, 38);
+            this.xHours.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xHours.Name = "xHours";
+            this.xHours.Size = new System.Drawing.Size(64, 50);
+            this.xHours.TabIndex = 0;
+            this.xHours.Text = "00";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(87, 143);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(97, 46);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // xMinutes
+            // 
+            this.xMinutes.AutoSize = true;
+            this.xMinutes.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xMinutes.Location = new System.Drawing.Point(207, 38);
+            this.xMinutes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xMinutes.Name = "xMinutes";
+            this.xMinutes.Size = new System.Drawing.Size(64, 50);
+            this.xMinutes.TabIndex = 2;
+            this.xMinutes.Text = "00";
+            // 
+            // xSeconds
+            // 
+            this.xSeconds.AutoSize = true;
+            this.xSeconds.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.xSeconds.Location = new System.Drawing.Point(324, 38);
+            this.xSeconds.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xSeconds.Name = "xSeconds";
+            this.xSeconds.Size = new System.Drawing.Size(64, 50);
+            this.xSeconds.TabIndex = 3;
+            this.xSeconds.Text = "00";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(192, 143);
+            this.pauseButton.Margin = new System.Windows.Forms.Padding(4);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(97, 46);
+            this.pauseButton.TabIndex = 4;
+            this.pauseButton.Text = " Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(297, 143);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(4);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(97, 46);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = " Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 50);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 38);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 50);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // taskName
+            // 
+            this.taskName.Location = new System.Drawing.Point(202, 97);
+            this.taskName.Margin = new System.Windows.Forms.Padding(4);
+            this.taskName.Name = "taskName";
+            this.taskName.Size = new System.Drawing.Size(141, 22);
+            this.taskName.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newTaskToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(472, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newTaskToolStripMenuItem
+            // 
+            this.newTaskToolStripMenuItem.Name = "newTaskToolStripMenuItem";
+            this.newTaskToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Task name: ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 118);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 326);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.taskName);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.xSeconds);
+            this.Controls.Add(this.xMinutes);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.xHours);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label xHours;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label xMinutes;
+        private System.Windows.Forms.Label xSeconds;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox taskName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newTaskToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ project/login/Form2.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con = new SqlConnection("Integrated Security=true;server=desktop-6rrc7gb;" +
+                                               "trusted_connection=yes;" +
+                                                "database=projectdb; " +
+                                                 "connection timeout=30");
+        SqlCommand cmd;
+
+        int seconds;
+        int minutes;
+        int hours;
+        Task currentTask = new Task();
+        bool isPaused = true;
+        DateTime startTime;
+        DateTime endTime;
+        DateTime pauseTime;
+        DateTime resumeTime;
+        double workingTime;
+        double nonWorkingTime;
+
+        string userIDvalue;
+
+        public Form2(string userID)
+        {
+            InitializeComponent();
+            seconds = minutes = hours = 0;
+            userIDvalue = userID;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            loadHoursTable();
+        }
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+
+            if(seconds > 59)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            if(minutes > 59)
+            {
+                hours++;
+                minutes = 0;
+            }
+
+            xHours.Text = appendZero(hours);
+            xMinutes.Text = appendZero(minutes);
+            xSeconds.Text = appendZero(seconds);
+
+        }
+
+        private string appendZero(double str)
+        {
+            if (str <= 9)
+                return "0" + str;
+            else
+                return str.ToString();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (taskName.Text != string.Empty) {
+
+                startTime = DateTime.Now;
+                currentTask = new Task(taskName.Text);
+                timer1.Start();
+                startButton.Enabled = false;
+                pauseButton.Enabled = true;
+                stopButton.Enabled = true;
+            } else {
+
+                MessageBox.Show("Please enter a task name");
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (isPaused) {
+
+                timer1.Stop();
+                pauseTime = DateTime.Now;
+                pauseButton.Text = "Resume";
+                stopButton.Enabled = false;
+                isPaused = false;
+            } else {
+
+                resumeTime = DateTime.Now;
+                pauseButton.Text = "Pause";
+                stopButton.Enabled = true;
+                timer1.Start();
+                isPaused = true;
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            endTime = DateTime.Now;
+           
+            TimeSpan breakHours = (resumeTime - pauseTime);
+            var breakTime = breakHours.ToString(@"hh\:mm\:ss");
+
+            TimeSpan hoursWorked = ((endTime - startTime) - (resumeTime - pauseTime));
+            var workedTime = hoursWorked.ToString(@"hh\:mm\:ss");
+
+            cmd = new SqlCommand("insert into Hours(workingHours, nonWorkingHours, task, employeeID) values(@workingHours, @nonWorkingHours, @task, @employeeID)", con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@workingHours", workedTime);
+            cmd.Parameters.AddWithValue("@nonWorkingHours", breakTime);
+            cmd.Parameters.AddWithValue("@task", taskName.Text);
+            cmd.Parameters.AddWithValue("@employeeID", userIDvalue);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully added record");
+
+            seconds = minutes = hours = 0;
+            xHours.Text = appendZero(hours);
+            xMinutes.Text = appendZero(minutes);
+            xSeconds.Text = appendZero(seconds);
+            taskName.Text = string.Empty;
+            startButton.Enabled = true;
+            pauseButton.Enabled = false;
+            stopButton.Enabled = false;
+
+            loadHoursTable();
+        }
+
+        private void loadHoursTable() {
+
+            cmd = new SqlCommand("SELECT * FROM Hours", con);
+            con.Open();
+            // SqlDataReader reader = cmd.ExecuteReader();
+
+            SqlDataAdapter data = new SqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            data.Fill(table);
+            dataGridView1.DataSource = new BindingSource(table, null);
+            // dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
